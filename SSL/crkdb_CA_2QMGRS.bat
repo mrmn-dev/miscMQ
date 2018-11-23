37d45d0e996cab60e0e8ab492dc2898cd41ed1af
mrmn-dev <mrmn@protonmail.com>
2018-11-22 19:11:33 -0600

    SSL

---
+++ SSL/crkdb_CA_2QMGRS.bat
@@ -0,0 +1,14 @@
+echo %JAVA_HOME%
+gsk7cmd -keydb -create -db qma_ssl.kdb -pw mqssl -type cms -expire 1095 -stash
+gsk7cmd -keydb -create -db qmb_ssl.kdb -pw mqssl -type cms -expire 1095 -stash
+gsk7cmd -keydb -create -db wmqca.kdb -pw mqssl -type cms -expire 1095 -stash
+gsk7cmd -cert -create -db wmqca.kdb -pw mqssl -label wmqca -dn " CN=MYCA, OU=WMQ, O=Curtis, L=Shoreview, ST=MN, C=US" -expire 1095
+gsk7cmd -cert -extract -db wmqca.kdb -pw mqssl -label wmqca -target wmqca.crt -format ascii
+gsk7cmd -cert -add -db qma_ssl.kdb -pw mqssl -label wmqca -file wmqca.crt -format ascii
+gsk7cmd -cert -add -db qmb_ssl.kdb -pw mqssl -label wmqca -file wmqca.crt -format ascii
+gsk7cmd -certreq -create -db qma_ssl.kdb -pw mqssl -label ibmwebspheremqqma_ssl -dn " CN=qma_ssl, OU=WMQ, O=Curtis, L=Shoreview, ST=MN, C=US" -file qm1req.arm
+gsk7cmd -certreq -create -db qmb_ssl.kdb -pw mqssl -label ibmwebspheremqqmb_ssl -dn " CN=qmb_ssl, OU=WMQ, O=Curtis, L=Shoreview, ST=MN, C=US" -file qm2req.arm
+gsk7cmd -cert -sign -file qm1req.arm -db wmqca.kdb -pw mqssl -label wmqca -target qm1cert.arm -format ascii -expire 1090
+gsk7cmd -cert -sign -file qm2req.arm -db wmqca.kdb -pw mqssl -label wmqca -target qm2cert.arm -format ascii -expire 1090
+gsk7cmd -cert -receive -db qma_ssl.kdb -pw mqssl -file qm1cert.arm -format ascii
+gsk7cmd -cert -receive -db qmb_ssl.kdb -pw mqssl -file qm2cert.arm -format ascii
---
+++ SSL/unstash.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#usage perl unstash.pl <Stash file name ex stash.sth>
+
+use strict;
+
+die "Usage: $0 <stash file>\n" if $#ARGV != 0;
+
+my $file=$ARGV[0];
+open(F,$file) || die "Can't open $file: $!";
+
+my $stash;
+read F,$stash,1024;
+
+my @unstash=map { $_^0xf5 } unpack("C*",$stash);
+
+foreach my $c (@unstash) {
+   last if $c eq 0;
+   printf "%c",$c;
+}
